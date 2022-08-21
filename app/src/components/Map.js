a0215d9e3cc7b7ee2afeec20b7ca249ed6599fdf
kelvinh <kevdagoat@gmail.com>
2022-08-21 15:17:41 +0800

    WIP: booking functionality

--- app/src/components/Map.js
+++ app/src/components/Map.js
@@ -1,30 +1,54 @@
 import React from "react";
 import axios from "axios";
-import hash from 'object-hash';
-import { MapContainer, TileLayer, FeatureGroup, Popup, Circle } from 'react-leaflet';
+import {
+  MapContainer,
+  TileLayer,
+  FeatureGroup,
+  Popup,
+  Circle,
+} from "react-leaflet";
 import ModalExample from "./Modal";
 import { dataURL } from "../config";
 
-
 function Map() {
   const [ferries, setFerries] = React.useState(null);
-  const [busstops, setbusstops] = React.useState(null);
-  const [modal, setModal] =  React.useState(false);
-  const [selectedFeature, setSelectedFeature] =  React.useState({});
+  const [modal, setModal] = React.useState(false);
+  const [selectedFeature, setSelectedFeature] = React.useState({});
+  const [ferryRatings, setFerryRatings] = React.useState({});
+
+  const updateRating = (pid, rating) => {
+    setFerryRatings((prevState) => {
+      // Deep copy state so we dont mutate
+      let newState = JSON.parse(JSON.stringify(prevState));
+      newState[pid] = rating;
+      return newState;
+    });
+  };
 
   const toggle = () => setModal(!modal);
-  //test request
-  //leaflet, mapbox, open street map, react leaflet
-  //example.features[0].geometry.coordinates
+
   React.useEffect(() => {
     axios.get(dataURL + "/ferries").then((response) => {
-        setFerries(response.data);
+      setFerries(response.data);
     });
-    /*axios.get(workDevUrl + "/busstops").then((response) => {
-        setbusstops(response.data);
-    });*/
   }, []);
+
+  React.useMemo(() => {
+    // Check if we need to populate feature ratings
+    if (Object.keys(ferryRatings).length < 1 && ferries) {
+      console.log("Fetching ferry ")
+      ferries.features.forEach((feature) => {
+        axios
+          .get(dataURL + "/bookings/" + feature.properties.LGIP_ID)
+          .then((response) => {
+            updateRating(feature.properties.LGIP_ID, response.data.count);
+          });
+      });
+    }
+  }, [ferries, ferryRatings]);
+
   if (!ferries) return null;
+
   return (
     <div className="Map">
       <link
@@ -34,14 +58,20 @@ function Map() {
         crossOrigin=""
       />
       {/*Below is because the long lat is in the order of lat long in geojson*/}
-      <MapContainer center={[ferries.features[0].geometry.coordinates[1], ferries.features[0].geometry.coordinates[0]]} zoom={10} scrollWheelZoom={true}>
-      <TileLayer
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-        {
-          ferries.features.map((feature,index) => {
-            return(
+      <MapContainer
+        center={[
+          ferries.features[0].geometry.coordinates[1],
+          ferries.features[0].geometry.coordinates[0],
+        ]}
+        zoom={10}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {ferries.features.map((feature, index) => {
+          return (
             <FeatureGroup color="purple" key={index}>
               <div class="flex w-5 h-5">
                 <Popup>
@@ -55,17 +85,22 @@ function Map() {
                     }}
                   >
                     <ModalExample
-                  modal={modal}
-                  toggle={toggle}
-                  selectedFeature={selectedFeature}
-                />
+                      modal={modal}
+                      toggle={toggle}
+                      selectedFeature={feature.properties.LGIP_ID}
+                      carbonRating={
+                        !ferryRatings[feature.properties.LGIP_ID]
+                          ? 0
+                          : ferryRatings[feature.properties.LGIP_ID]
+                      }
+                    />
                   </button>
                 </Popup>
               </div>
               <Circle
                 center={[
                   feature.geometry.coordinates[1],
-                  feature.geometry.coordinates[0]
+                  feature.geometry.coordinates[0],
                 ]}
                 fillColor="#0020ff"
                 radius={50}
@@ -75,12 +110,9 @@ function Map() {
                 fillOpacity={0.8}
               />
             </FeatureGroup>
-          )
-        })
-        }
-        {//<GeoJSON key={hash(busstops)} data={busstops} />
-        }
-    </MapContainer>
+          );
+        })}
+      </MapContainer>
     </div>
   );
 }
--- app/src/components/Modal.js
+++ app/src/components/Modal.js
@@ -1,11 +1,28 @@
 import LeafRating from "./LeafRating";
-import { calcCarbon } from "../functions/calculations";
 import React from "react";
-const ModalExample = ({ modal, toggle, selectedFeature }) => {
+import axios from "axios";
+import { dataURL } from "../config";
+
+const ModalExample = ({ modal, toggle, selectedFeature, carbonRating }) => {
+  const [booked, setBooked] = React.useState(false);
+
+  const bookFeature = () => {
+    axios.post(dataURL + "/addbooking/" + selectedFeature).then(() => {
+      setBooked(true);
+    });
+  };
+
   return (
     <div>
-      <p>Carbon Rating</p> 
-      <LeafRating carbonRating={calcCarbon()}/>
+      <p>Carbon Rating</p>
+      <LeafRating carbonRating={carbonRating} />
+      {booked ? (
+        <p>Your booking has been recorded.</p>
+      ) : (
+        <button className="border br-2 p-2" onClick={bookFeature}>
+          Book Now
+        </button>
+      )}
     </div>
   );
 };
